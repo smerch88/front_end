4cfe17f4a50612e670a282ff4964c34dedb5db04
smerch88 <maxarmyk@gmail.com>
2022-08-04 15:50:41 +0300

    Added first task

--- ДЗ 2. 3 Условные операторы/Задание 1/index.js
+++ ДЗ 2. 3 Условные операторы/Задание 1/index.js
@@ -2,26 +2,30 @@
 // Верно ли, что все цифры одинаковы? ‘333’
 // Есть ли среди цифр цифры одинаковые? ‘332’ ‘233’ ‘323’
 
-const variable_1 = String(parseInt(prompt("Enter a number")));
-let step;
-let counter_of_same_numbers = 1;
 
-for (step = 0; step < variable_1.length - 1; step++) {
-    if (variable_1.charAt(step) == variable_1.charAt(step + 1 )) {
-        counter_of_same_numbers += 1;      
-        } 
-    else {
-        console.log("All digits of a number are not equal."); 
-        break;
-    }
-    if (counter_of_same_numbers == variable_1.length) {
-        console.log("All digits of a number are equal.");
-    }
-}
+const variable_1 = parseInt(prompt("Enter a number"));
+let first_digit_var;
+let second_digit_var;
+let third_digit_var;
 
-if (variable_1.charAt(0) == variable_1.charAt(1) == variable_1.charAt(2)){
+third_digit_var = variable_1 % 10;
+second_digit_var = ((variable_1 - third_digit_var) / 10) % 10;
+first_digit_var = (variable_1 - variable_1 % 100) / 100;
+
+console.log(first_digit_var, second_digit_var, third_digit_var)
+
+if (first_digit_var == second_digit_var == third_digit_var) {    
     console.log("All digits of a number are equal.");
 }
-else if (variable_1.charAt(0) == variable_1.charAt(1) && variable_1.charAt(0) != variable_1.charAt(2)){
-    console.log("All digits of a number are equal.");
+else if (first_digit_var == second_digit_var && first_digit_var != third_digit_var){
+    console.log("First and second digits are equal.");
+}
+else if (first_digit_var == third_digit_var && first_digit_var != second_digit_var){
+    console.log("First and third digits are equal.");
+}
+else if (second_digit_var == third_digit_var && first_digit_var != second_digit_var){
+    console.log("Second and third digits are equal.");
+}
+else {
+    console.log("No equal digits in the number.");
 }
