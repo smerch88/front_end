07f8541e07f622672b1fdc6766ac7748a749b99a
smerch88 <maxarmyk@gmail.com>
2022-08-04 16:55:32 +0300

    hw 2 full
    added comments

--- ДЗ 2. 3 Условные операторы/Задание 2/index.js
+++ ДЗ 2. 3 Условные операторы/Задание 2/index.js
@@ -2,9 +2,12 @@
 
 const variable = parseInt(prompt("Enter a 6-digit number"));
 
+// получаем вторую половину числа, это остаток от деления на 1000
 let second_part_var = variable % 1000;
+// получаем первую часть числа, это всё число минус 2 часть разделить на 1000 
 let first_part_var = (variable - second_part_var) / 1000;
-let second_part_var_reversed = (second_part_var % 10 * 100) + ( second_part_var % 100 -  second_part_var % 10) + (second_part_var - (second_part_var % 100))/100;
+// переворачиваем 2 часть числа | последняя цифра * 100 | остаток от деления на 100 - остаток от деления на 10 | 2 часть изначального числа - остаток от деления на 100 и убрать нули 
+let second_part_var_reversed = (second_part_var % 10 * 100) + ( second_part_var % 100 -  second_part_var % 10) + (second_part_var - (second_part_var % 100))/100; 
 
 if (first_part_var == second_part_var_reversed) {
     console.log("The number is a palindrom")
