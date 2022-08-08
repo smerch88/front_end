4c8e48e70d4849e66c7f69de136ca38214415a29
smerch88 <maxarmyk@gmail.com>
2022-08-08 18:09:58 +0300

    some bugs fixed

--- ДЗ 3. Циклы/Задание 4/index.js
+++ ДЗ 3. Циклы/Задание 4/index.js
@@ -1,24 +1,24 @@
 //  Дано целое число (ввести через prompt). Выяснить, является ли оно простым (простым называется число, больше 1, не имеющих других делителей кроме 1 и себя).
 
 let number_var = parseInt(prompt("Enter a number:"));
-let i = 0;
+let counter = 0;
 
 
-    for( let j = 0; j <= (Math.ceil(number_var/2)); j++){
-        if ((number_var % j) == 0){
-            console.log("number_var % j = ",number_var % j)
-            i += 1;
-        }
+for (let j = 0; j <= (Math.ceil(number_var / 2)); j++) {
+    if ((number_var % j) == 0) {
+        console.log("Debug: number_var % j = ", number_var % j)
+        counter += 1;
     }
+}
 
 
-console.log("Math.ceil(number_var/2) ", Math.ceil(number_var/2))
-console.log("i = ", i)
+console.log("Debug: Math.ceil(number_var/2) ", Math.ceil(number_var / 2))
+console.log("Debug: i = ", counter)
 
 
-if (i==1){
+if (counter == 1) {
     console.log("Number is Prime")
 }
-else{
+else {
     console.log("Number is not Prime")
 }
