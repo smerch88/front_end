caa45cd69e9b1f1ac8d199b16974911a76ff73c4
smerch88 <maxarmyk@gmail.com>
2022-08-08 21:50:52 +0300

    renamed constants, initialize users data as const parameters

--- ДЗ 3. Циклы/Задание 3/index.js
+++ ДЗ 3. Циклы/Задание 3/index.js
@@ -1,15 +1,15 @@
 // Дано целое число N (ввести через prompt). Вывести все целые числа от 1 до 100, квадрат которых не превышает числа N. Если N не является числом, то вывести ошибку.
 
-let number_var = parseInt(prompt("Enter a number:"));
+const number = parseInt(prompt("Enter a number:"));
 
-if (isNaN(number_var)){
+if (isNaN(number)){
     console.log("Error input is not a number")
 }
 else {    
     let i = 1;
     let j = 100;
 
-    while ((i**2<=  number_var) && j>0) {
+    while ((i**2<=  number) && j>0) {
         console.log("Square of ", i,"=",i**2, "so it satisfies requirements.")
         i += 1;
         j -=1;
--- ДЗ 3. Циклы/Задание 4/index.js
+++ ДЗ 3. Циклы/Задание 4/index.js
@@ -1,22 +1,22 @@
 //  Дано целое число (ввести через prompt). Выяснить, является ли оно простым (простым называется число, больше 1, не имеющих других делителей кроме 1 и себя).
 
-let number_var = parseInt(prompt("Enter a number:"));
+const number = parseInt(prompt("Enter a number:"));
 let counter = 0;
 
 
-for (let j = 0; j <= (Math.ceil(number_var / 2)); j++) {
-    if ((number_var % j) == 0) {
-        console.log("Debug: number_var % j = ", number_var % j)
+for (let j = 0; j <= (Math.ceil(number / 2)); j++) {
+    if ((number % j) == 0) {
+        console.log("Debug: number % j = ", number % j)
         counter += 1;
     }
 }
 
 
-console.log("Debug: Math.ceil(number_var/2) ", Math.ceil(number_var / 2))
+console.log("Debug: Math.ceil(number/2) ", Math.ceil(number / 2))
 console.log("Debug: counter = ", counter)
 
 
-if (counter == 1 && number_var != 1) {
+if (counter == 1 && number != 1) {
     console.log("Number is Prime")
 }
 else {
