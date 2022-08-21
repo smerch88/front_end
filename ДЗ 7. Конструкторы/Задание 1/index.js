c957451ab0adec83873729d82114a259746c348a
smerch88 <maxarmyk@gmail.com>
2022-08-21 15:30:50 +0300

    done

---
+++ ДЗ 7. Конструкторы/Задание 1/index.js
@@ -0,0 +1,29 @@
+// 1. Создать сущность человека (данные вводит пользователь)
+
+
+
+// имя
+// возраст
+// Метод вывода данных об объекте
+
+
+function User() {
+  this.name = prompt("Your name?", "Vasya");
+  this.age = prompt("Your age?", 18);
+  this.gender = prompt("Your gender?", "male");
+ }
+
+
+let user = new User();
+console.log(user);
+
+// let user2 = new User();
+// console.log(user2);
+
+function showData(userName){
+  for (item in userName){
+    console.log(item, ":" ,userName[item]);
+  }
+}
+
+showData(user);
---
+++ ДЗ 7. Конструкторы/Задание 2/index.js
@@ -0,0 +1,59 @@
+// 2. Создать сущность автомобиля:
+
+// Характеристики автомобиля отдельными свойствами, 3 свойства
+
+// Методы:
+
+// Вывод на экран данных об этом автомобиле
+// Присвоение этого автомобиля владельцу (записать в автомобиль объект владельца)
+// Все данные о человеке и автомобиле получать от пользователя. Реализовать необходимые проверки на корректность ввода (пустые поля, возраст >18 для человека и т.д. в случае необходимости).
+// Максимально использовать функции
+
+function User() {
+    this.name = prompt("Your name?", "Enter name here");
+    this.age = prompt("Your age?", 18);
+}
+  
+
+function Car(user) {
+    if (user.age >= 18){
+        this.owner = user.name;
+        this.brand = empyFieldStr("Brand name:");
+        this.year = empyFieldStr("Car year:");
+        this.tires = empyFieldStr("Tires:");
+        this.power = empyFieldStr("Engine power");
+    }
+    else alert("You are not allowed to derive a car");
+}
+
+
+let user = new User();
+let car = new Car(user);
+
+  
+function showData(userName){
+    console.log("______________________")
+    for (item in userName){
+        if (userName[item]!==null && userName[item]!==false){
+        console.log(item, ":" ,userName[item]);
+        }
+    }
+    console.log("______________________")
+}
+
+
+function empyFieldStr(msg){
+    let text;
+    do {
+        text = prompt(msg);
+        if (text === null ){
+            text = false;
+        };
+      } while (text.length === 0);
+    return text;
+};
+
+
+showData(user);
+showData(car);
+console.log(car)
