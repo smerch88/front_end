978e6009fc9cfa4f5d53626d8a9faa716227c0fc
smerch88 <maxarmyk@gmail.com>
2022-09-05 23:59:40 +0300

    Added several improvements

--- ДЗ 10. ToDo List/index.js
+++ ДЗ 10. ToDo List/index.js
@@ -25,50 +25,83 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
   if (this.elements[0].value.length === 0) {
     alert("You cant add empty field!");
+    return;
   } else if (this.elements[0].value.length > 20) {
     alert("Your task is too long!");
-  } else {
-    let div = document.createElement("div");
-    div.setAttribute("class", "wrapper");
-    document.body.appendChild(div);
+    return;
+  }
 
-    text = this.elements[0].value;
-    const field = document.createElement("p"); //добавления текста из формы
-    field.innerText = text;
-    div.appendChild(field);
+  // let div = document.createElement("div");
+  // div.setAttribute("class", "wrapper");
+  // document.body.appendChild(div);
 
-    const checkbox = document.createElement("input"); //добавление чекбокса
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("name", "checkbox");
-    checkbox.setAttribute("class", "checkbox");
-    div.appendChild(checkbox);
+  const div = createNode("div", [{ name: "class", value: "wrapper" }]);
+  document.body.appendChild(div);
 
-    const removeBtn = document.createElement("button"); //добавдение кнопки удалить
-    removeBtn.setAttribute("id", `btnRemoveTask${i}`);
-    removeBtn.setAttribute("class", "removeBtn");
-    removeBtn.innerText = "Remove";
-    div.appendChild(removeBtn);
+  text = this.elements[0].value;
+  // const field = document.createElement("p"); //добавления текста из формы
+  // field.innerText = text;
+  // div.appendChild(field);
 
-    const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
-    btnRemoveTask.addEventListener("click", removeField);
+  const field = createNode("p", []);
+  field.innerText = text;
+  div.appendChild(field);
 
-    checkbox.addEventListener("change", function () {
-      //чекбокс перечеркивает текст
-      if (this.checked) {
-        field.setAttribute("class", "line-through ");
-        console.log("Checkbox is checked..");
-      } else {
-        field.setAttribute("class", "");
-        console.log("Checkbox is not checked..");
-      }
-    });
+  // const checkbox = document.createElement("input"); //добавление чекбокса
+  // checkbox.setAttribute("type", "checkbox");
+  // checkbox.setAttribute("name", "checkbox");
+  // checkbox.setAttribute("class", "checkbox");
+  // div.appendChild(checkbox);
 
-    function removeField() {
-      //удаляет 3 елемента задания
-      field.remove();
-      removeBtn.remove();
-      checkbox.remove();
-      div.remove();
+  const checkbox = createNode("input", [
+    { name: "type", value: "checkbox" },
+    { name: "name", value: "checkbox" },
+    { name: "class", value: "checkbox" },
+  ]);
+  div.appendChild(checkbox);
+
+  // const removeBtn = document.createElement("button"); //добавдение кнопки удалить
+  // removeBtn.setAttribute("id", `btnRemoveTask${i}`);
+  // removeBtn.setAttribute("class", "removeBtn");
+  // removeBtn.innerText = "Remove";
+  // div.appendChild(removeBtn);
+
+  const removeBtn = createNode("button", [
+    { name: "id", value: `btnRemoveTask${i}` },
+    { name: "class", value: "removeBtn" },
+  ]);
+  removeBtn.innerText = "Remove";
+  div.appendChild(removeBtn);
+
+  const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
+  btnRemoveTask.addEventListener("click", removeField);
+
+  checkbox.addEventListener("change", function () {
+    //чекбокс перечеркивает текст
+    if (this.checked) {
+      field.setAttribute("class", "line-through ");
+      console.log("Checkbox is checked..");
+    } else {
+      field.setAttribute("class", "");
+      console.log("Checkbox is not checked..");
     }
+  });
+
+  this.elements[0].value = ""; //обнуляет поле ввода
+
+  function removeField() {
+    //удаляет 3 елемента задания
+    field.remove();
+    removeBtn.remove();
+    checkbox.remove();
+    div.remove();
   }
 });
+
+function createNode(tagName, attributes) {
+  const el = document.createElement(tagName);
+  attributes.forEach(({ name, value }) => {
+    el.setAttribute(name, value);
+  });
+  return el;
+}
